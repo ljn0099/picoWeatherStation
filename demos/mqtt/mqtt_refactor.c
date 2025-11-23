4871b6442f372fb366c280beb82985b7836115d9
Manuel <ljn0099@hotmail.com>
2025-11-23 13:38:37 +0100

    Added new mqtt demo

--- demos/mqtt/mqtt_refactor.c
+++ demos/mqtt/mqtt_refactor.c
@@ -12,13 +12,22 @@
 #error Need to define MQTT_SERVER
 #endif
 
+#define UNIQUE_ID_HEX_LEN (2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1)
+
 // This file includes your client certificate for client server authentication
 #ifdef MQTT_CERT_INC
 #include MQTT_CERT_INC
 #endif
 
+#define DNS_RETRY_MS 5000
+#define MQTT_RETRY_MS 5000
+
 #ifndef MQTT_TOPIC_LEN
 #define MQTT_TOPIC_LEN (sizeof(MQTT_USERNAME) + 50)
+#endif
+
+#ifndef MQTT_DEVICE_NAME
+#define MQTT_DEVICE_NAME "pico"
 #endif
 
 typedef struct {
@@ -26,6 +35,8 @@ typedef struct {
     struct mqtt_connect_client_info_t mqttClientInfo;
     char data[MQTT_OUTPUT_RINGBUF_SIZE];
     char topic[MQTT_TOPIC_LEN];
+    char clientId[((sizeof(MQTT_DEVICE_NAME) - 1) + 1 + (UNIQUE_ID_HEX_LEN - 1) + 1)];
+    char willTopic[MQTT_TOPIC_LEN];
     uint32_t len;
     ip_addr_t mqttServerAddress;
     bool connectDone;
@@ -70,16 +81,47 @@ typedef struct {
 #define MQTT_WILL_MSG "0"
 #define MQTT_WILL_QOS 1
 
-#ifndef MQTT_DEVICE_NAME
-#define MQTT_DEVICE_NAME "pico"
-#endif
-
 #define MQTT_SERVER_PORT 8883
 
 static const char *full_topic(const char *name) {
     static char full_topic[MQTT_TOPIC_LEN];
     snprintf(full_topic, sizeof(full_topic), "%s%s", MQTT_USERNAME, name);
     return full_topic;
+}
+
+static void pub_request_cb(__unused void *arg, err_t err) {
+    if (err != 0) {                                        
+        ERROR_printf("pub_request_cb failed %d", err);     
+    }                                                      
+}
+
+static void sub_request_cb(void *arg, err_t err) {
+    mqtt_t *state = (mqtt_t *)arg;
+    if (err != 0) {
+        ERROR_printf("subscribe request failed %d", err);
+        hard_assert(async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(),
+                                               &state->requestWorker, MQTT_RETRY_MS));
+        return;
+    }
+    state->subscribeCount++;
+}
+
+static void unsub_request_cb(void *arg, err_t err) {
+    mqtt_t *state = (mqtt_t *)arg;
+    if (err != 0) {
+        ERROR_printf("unsubscribe request failed %d", err);
+        hard_assert(async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(),
+                                               &state->requestWorker, MQTT_RETRY_MS));
+        return;
+    }
+    state->subscribeCount--;
+    assert(state->subscribeCount >= 0);
+}
+
+static void sub_unsub_topics(mqtt_t *state, bool sub) {
+    mqtt_request_cb_t cb = sub ? sub_request_cb : unsub_request_cb;
+    mqtt_sub_unsub(state->mqttClientInst, full_topic("/ping"), MQTT_SUBSCRIBE_QOS, cb,
+                   state, sub);
 }
 
 static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags) {
@@ -93,12 +135,12 @@ static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t f
 
     DEBUG_printf("Topic: %s, Message: %s\n", state->topic, state->data);
 
-    // if (strcmp(basic_topic, "/ping") == 0) {
-    //     char buf[11];
-    //     snprintf(buf, sizeof(buf), "%lu", to_ms_since_boot(get_absolute_time()) / 1000);
-    //     mqtt_publish(state->mqttClientInst, full_topic("/uptime"), buf, strlen(buf),
-    //                  MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
-    // }
+    if (strcmp(basic_topic, "/ping") == 0) {
+        char buf[11];
+        snprintf(buf, sizeof(buf), "%lu", to_ms_since_boot(get_absolute_time()) / 1000);
+        mqtt_publish(state->mqttClientInst, full_topic("/uptime"), buf, strlen(buf),
+                     MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+    }
 }
 
 static void mqtt_incoming_publish_cb(void *arg, const char *topic, uint32_t totLen) {
@@ -110,37 +152,40 @@ static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection
     mqtt_t *state = (mqtt_t *)arg;
     if (status == MQTT_CONNECT_ACCEPTED) {
         state->connectDone = true;
-        // sub_unsub_topics(state, true); // subscribe;
+        sub_unsub_topics(state, true); // subscribe;
 
         // indicate online
-        // if (state->mqtt_client_info.will_topic) {
-        //     mqtt_publish(state->mqtt_client_inst, state->mqtt_client_info.will_topic, "1", 1,
-        //                  MQTT_WILL_QOS, true, pub_request_cb, state);
-        // }
-
-        // Publish temperature every 10 sec if it's changed
-        // temperature_worker.user_data = state;
-        // async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(), &temperature_worker,
-        // 0);
-    }
-    else if (status == MQTT_CONNECT_DISCONNECTED) {
-        if (!state->connectDone) {
-            panic("Failed to connect to mqtt server");
+        if (state->mqttClientInfo.will_topic) {
+            mqtt_publish(state->mqttClientInst, state->mqttClientInfo.will_topic, "1", 1,
+                         MQTT_WILL_QOS, true, pub_request_cb, state);
         }
+
     }
-    else {
-        panic("Unexpected status");
+    else { 
+        DEBUG_printf("MQTT disconnected (status %d), reconnecting...\n", status);
+        state->connectDone = false;
+        hard_assert(async_context_add_at_time_worker_in_ms(
+            cyw43_arch_async_context(), &state->requestWorker, MQTT_RETRY_MS));
     }
 }
 
 static void mqtt_start_client(mqtt_t *state) {
+    state->mqttClientInst = mqtt_client_new();
+    if (!state->mqttClientInst) {
+        panic("MQTT client instance creation error\n");
+    }
+
     INFO_printf("IP address of this device %s\n", ipaddr_ntoa(&(netif_list->ip_addr)));
     INFO_printf("Connecting to mqtt server at %s\n", ipaddr_ntoa(&state->mqttServerAddress));
 
     cyw43_arch_lwip_begin();
     if (mqtt_client_connect(state->mqttClientInst, &state->mqttServerAddress, MQTT_SERVER_PORT,
                             mqtt_connection_cb, state, &state->mqttClientInfo) != ERR_OK) {
-        panic("MQTT broker connection error");
+        DEBUG_printf("MQTT broker connection error\n");
+        hard_assert(async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(),
+                                                   &state->requestWorker, MQTT_RETRY_MS));
+        cyw43_arch_lwip_end();
+        return;
     }
     // This is important for MBEDTLS_SSL_SERVER_NAME_INDICATION
     mbedtls_ssl_set_hostname(altcp_tls_context(state->mqttClientInst->conn), MQTT_SERVER);
@@ -158,9 +203,9 @@ static void mqtt_dns_found(const char *hostname, const ip_addr_t *ipaddr, void *
         mqtt_start_client(state);
     }
     else {
-        panic("mqtt dns request failed\n");
-        // DEBUG_printf("mqtt dns request failed\n");
-        // ntp_result(state, -1, NULL);
+        DEBUG_printf("mqtt dns request failed\n");
+        hard_assert(async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(),
+                                                           &state->requestWorker, DNS_RETRY_MS));
     }
 }
 
@@ -172,37 +217,29 @@ static void request_worker_fn(__unused async_context_t *context, async_at_time_w
         mqtt_start_client(state);
     }
     else if (err != ERR_INPROGRESS) { // ERR_INPROGRESS means expect a callback
-        panic("dns request failed\n");
-        // DEBUG_printf("dns request failed\n");
-        // ntp_result(state, -1, NULL);
+        DEBUG_printf("dns request failed\n");
+        hard_assert(async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(),
+                                                           &state->requestWorker, DNS_RETRY_MS));
     }
 }
 
-static mqtt_t *mqtt_init(const char *uniqueId) {
+static mqtt_t *mqtt_init() {
     mqtt_t *state = (mqtt_t *)calloc(1, sizeof(mqtt_t));
     if (!state) {
         DEBUG_printf("failed to allocate state for mqtt\n");
         return NULL;
     }
 
-    state->mqttClientInst = mqtt_client_new();
-    if (!state->mqttClientInst) {
-        DEBUG_printf("MQTT client instance creation error");
-        return NULL;
-    }
-
     state->requestWorker.do_work = request_worker_fn;
     state->requestWorker.user_data = state;
 
-    state->mqttClientInfo.client_id = uniqueId;
     state->mqttClientInfo.keep_alive = MQTT_KEEP_ALIVE_S; // Keep alive in sec
 
     state->mqttClientInfo.client_user = MQTT_USERNAME;
     state->mqttClientInfo.client_pass = MQTT_PASSWORD;
 
-    static char will_topic[MQTT_TOPIC_LEN];
-    strncpy(will_topic, full_topic(MQTT_WILL_TOPIC), sizeof(will_topic));
-    state->mqttClientInfo.will_topic = will_topic;
+    snprintf(state->willTopic, sizeof(state->willTopic), "%s%s", MQTT_USERNAME, MQTT_WILL_TOPIC);
+    state->mqttClientInfo.will_topic = state->willTopic;
     state->mqttClientInfo.will_msg = MQTT_WILL_MSG;
     state->mqttClientInfo.will_qos = MQTT_WILL_QOS;
     state->mqttClientInfo.will_retain = true;
@@ -216,20 +253,17 @@ static mqtt_t *mqtt_init(const char *uniqueId) {
 
 void mqtt_start(void) {
     // Use board unique id
-    char unique_id_buf[5];
-    pico_get_unique_board_id_string(unique_id_buf, sizeof(unique_id_buf));
-    for (int i = 0; i < sizeof(unique_id_buf) - 1; i++) {
-        unique_id_buf[i] = tolower(unique_id_buf[i]);
+    char uniqueIdBuf[UNIQUE_ID_HEX_LEN];
+
+    pico_get_unique_board_id_string(uniqueIdBuf, sizeof(uniqueIdBuf));
+    for (int i = 0; i < sizeof(uniqueIdBuf) - 1; i++) {
+        uniqueIdBuf[i] = tolower(uniqueIdBuf[i]);
     }
 
-    // Generate a unique name, e.g. pico1234
-    char client_id_buf[sizeof(MQTT_DEVICE_NAME) + sizeof(unique_id_buf) - 1];
-    memcpy(&client_id_buf[0], MQTT_DEVICE_NAME, sizeof(MQTT_DEVICE_NAME) - 1);
-    memcpy(&client_id_buf[sizeof(MQTT_DEVICE_NAME) - 1], unique_id_buf, sizeof(unique_id_buf) - 1);
-    client_id_buf[sizeof(client_id_buf) - 1] = 0;
-    INFO_printf("Device name %s\n", client_id_buf);
+    mqtt_t *state = mqtt_init();
 
-    mqtt_t *state = mqtt_init(client_id_buf);
+    snprintf(state->clientId, sizeof(state->clientId), "%s-%s", MQTT_DEVICE_NAME, uniqueIdBuf);
+    state->mqttClientInfo.client_id = state->clientId;
 
     hard_assert(async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(),
                                                        &state->requestWorker, 0));
