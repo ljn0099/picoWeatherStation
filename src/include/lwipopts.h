f291a7e3cbe324ebd159d24e05a139081f400e1b
Manuel <ljn0099@hotmail.com>
2025-11-23 14:02:09 +0100

    updated lwipopts.h config

--- src/include/lwipopts.h
+++ src/include/lwipopts.h
@@ -1,8 +1,11 @@
 #ifndef _LWIPOPTS_H
 #define _LWIPOPTS_H
 
-
-// Common settings used in most of the pico_w examples
+// Need more memory for TLS
+#ifdef MQTT_CERT_INC
+#define MEM_SIZE 8000
+#endif
+// Common settings
 // (see https://www.nongnu.org/lwip/2_1_x/group__lwip__opts.html for details)
 
 // allow override in some examples
@@ -89,4 +92,22 @@
 #define SLIP_DEBUG                  LWIP_DBG_OFF
 #define DHCP_DEBUG                  LWIP_DBG_OFF
 
-#endif /* __LWIPOPTS_H__ */
+#define MEMP_NUM_SYS_TIMEOUT        (LWIP_NUM_SYS_TIMEOUT_INTERNAL+1)
+
+#ifdef MQTT_CERT_INC
+#define LWIP_ALTCP               1
+#define LWIP_ALTCP_TLS           1
+#define LWIP_ALTCP_TLS_MBEDTLS   1
+#ifndef NDEBUG
+#define ALTCP_MBEDTLS_DEBUG  LWIP_DBG_ON
+#endif
+/* TCP WND must be at least 16 kb to match TLS record size
+   or you will get a warning "altcp_tls: TCP_WND is smaller than the RX decrypion buffer, connection RX might stall!" */
+#undef TCP_WND
+#define TCP_WND  16384
+#endif // MQTT_CERT_INC
+
+// This defaults to 4
+#define MQTT_REQ_MAX_IN_FLIGHT 5
+
+#endif
