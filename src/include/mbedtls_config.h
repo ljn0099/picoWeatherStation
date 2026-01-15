712e1b4fc81d8258cfcbe27d935fcd309d8dcf12
Manuel <ljn0099@hotmail.com>
2026-01-15 17:50:24 +0100

    Disabled TLS1.2 supported and added coments to config

--- src/include/mbedtls_config.h
+++ src/include/mbedtls_config.h
@@ -16,15 +16,21 @@
 #define MBEDTLS_PLATFORM_TIME_ALT
 
 #define MBEDTLS_HAVE_ASM
+
+// Ciphers
 #define MBEDTLS_CIPHER_MODE_CBC
 #define MBEDTLS_CIPHER_MODE_CFB
 #define MBEDTLS_CIPHER_MODE_CTR
 #define MBEDTLS_CIPHER_MODE_OFB
 #define MBEDTLS_CIPHER_MODE_XTS
+
+// Support for specific padding modes in the cipher layer.
 #define MBEDTLS_CIPHER_PADDING_PKCS7
 #define MBEDTLS_CIPHER_PADDING_ONE_AND_ZEROS
 #define MBEDTLS_CIPHER_PADDING_ZEROS_AND_LEN
 #define MBEDTLS_CIPHER_PADDING_ZEROS
+
+// Enable specific cuvers within the Elliptic Curve Module
 #define MBEDTLS_ECP_DP_SECP192R1_ENABLED
 #define MBEDTLS_ECP_DP_SECP224R1_ENABLED
 #define MBEDTLS_ECP_DP_SECP256R1_ENABLED
@@ -36,48 +42,95 @@
 #define MBEDTLS_ECP_DP_BP256R1_ENABLED
 #define MBEDTLS_ECP_DP_BP384R1_ENABLED
 #define MBEDTLS_ECP_DP_BP512R1_ENABLED
+// Montgomery curves (supporting ECP)
 #define MBEDTLS_ECP_DP_CURVE25519_ENABLED
 #define MBEDTLS_ECP_DP_CURVE448_ENABLED
+
+// Use nist optimizations for Elliptic Curve
 #define MBEDTLS_ECP_NIST_OPTIM
+
+// Enable deterministic ECDSA
 #define MBEDTLS_ECDSA_DETERMINISTIC
+
+// Enable the PSK based ciphersuite modes in SSL / TLS
 #define MBEDTLS_KEY_EXCHANGE_PSK_ENABLED
+
+// Enable the DHE-PSK based ciphersuite modes in SSL / TLS
 #define MBEDTLS_KEY_EXCHANGE_DHE_PSK_ENABLED
+
+// Enable the ECDHE-PSK based ciphersuite modes in SSL / TLS
 #define MBEDTLS_KEY_EXCHANGE_ECDHE_PSK_ENABLED
+
+// Enable the RSA-PSK based ciphersuite modes in SSL / TLS.
 #define MBEDTLS_KEY_EXCHANGE_RSA_PSK_ENABLED
+
+// Enable the RSA-only based ciphersuite modes in SSL / TLS
 #define MBEDTLS_KEY_EXCHANGE_RSA_ENABLED
+
+// Enable the DHE-RSA based ciphersuite modes in SSL / TLS
 #define MBEDTLS_KEY_EXCHANGE_DHE_RSA_ENABLED
+
+// Enable the ECDHE-RSA based ciphersuite modes in SSL / TLS.
 #define MBEDTLS_KEY_EXCHANGE_ECDHE_RSA_ENABLED
+
+// Enable the ECDHE-ECDSA based ciphersuite modes in SSL / TLS.
 #define MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED
+
+// Enable the ECDH-ECDSA based ciphersuite modes in SSL / TLS.
 #define MBEDTLS_KEY_EXCHANGE_ECDH_ECDSA_ENABLED
+
+// Enable the ECDH-RSA based ciphersuite modes in SSL / TLS.
 #define MBEDTLS_KEY_EXCHANGE_ECDH_RSA_ENABLED
+
+// Enhance support for reading EC keys using variants of SEC1 not allowed by RFC 5915 and RFC 5480.
 #define MBEDTLS_PK_PARSE_EC_EXTENDED
+
+// Enable the support for parsing public keys of type Short Weierstrass
+// (MBEDTLS_ECP_DP_SECP_XXX and MBEDTLS_ECP_DP_BP_XXX) which are using the
+// compressed point format. This parsing is done through ECP module's functions.
 #define MBEDTLS_PK_PARSE_EC_COMPRESSED
+
+// Enable a dummy error function to make use of mbedtls_strerror() in
+// third party libraries easier when MBEDTLS_ERROR_C is disabled
 #define MBEDTLS_ERROR_STRERROR_DUMMY
+
+// Enable the prime-number generation code.
 #define MBEDTLS_GENPRIME
+
+// Support external private RSA keys (eg from a HSM) in the PK layer.
 #define MBEDTLS_PK_RSA_ALT_SUPPORT
+
+// Enable support for PKCS#1 v1.5 encoding.
 #define MBEDTLS_PKCS1_V15
+
+// Enable support for PKCS#1 v2.1 encoding.
 #define MBEDTLS_PKCS1_V21
+
+// Enable the checkup functions (*_self_test).
 #define MBEDTLS_SELF_TEST
+
+// Enable sending of alert messages in case of encountered errors as per RFC.
 #define MBEDTLS_SSL_ALL_ALERT_MESSAGES
-#define MBEDTLS_SSL_DTLS_CONNECTION_ID
-#define MBEDTLS_SSL_DTLS_CONNECTION_ID_COMPAT 0
+
+// Enable serialization of the TLS context structures, through use of the
+// functions mbedtls_ssl_context_save() and mbedtls_ssl_context_load().
 #define MBEDTLS_SSL_CONTEXT_SERIALIZATION
+
+// This allows peers that both support it to use a more robust protection for
+// ciphersuites using CBC, providing deep resistance against timing attacks
+// on the padding or underlying cipher.
 #define MBEDTLS_SSL_ENCRYPT_THEN_MAC
+
 #define MBEDTLS_SSL_EXTENDED_MASTER_SECRET
 #define MBEDTLS_SSL_KEEP_PEER_CERTIFICATE
 #define MBEDTLS_SSL_RENEGOTIATION
 #define MBEDTLS_SSL_MAX_FRAGMENT_LENGTH
-#define MBEDTLS_SSL_PROTO_TLS1_2
 #define MBEDTLS_SSL_PROTO_TLS1_3
 #define MBEDTLS_SSL_TLS1_3_COMPATIBILITY_MODE
 #define MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_PSK_ENABLED
 #define MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_EPHEMERAL_ENABLED
 #define MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_PSK_EPHEMERAL_ENABLED
-#define MBEDTLS_SSL_PROTO_DTLS
 #define MBEDTLS_SSL_ALPN
-#define MBEDTLS_SSL_DTLS_ANTI_REPLAY
-#define MBEDTLS_SSL_DTLS_HELLO_VERIFY
-#define MBEDTLS_SSL_DTLS_CLIENT_PORT_REUSE
 #define MBEDTLS_SSL_SERVER_NAME_INDICATION
 #define MBEDTLS_VERSION_FEATURES
 #define MBEDTLS_X509_RSASSA_PSS_SUPPORT
