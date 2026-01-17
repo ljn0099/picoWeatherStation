6dda241cd29ecd9cb4ecd632d33a6b1345b0a7a0
Manuel <ljn0099@hotmail.com>
2026-01-18 00:41:03 +0100

    Changed printf to debug printf

--- src/main.c
+++ src/main.c
@@ -461,7 +461,7 @@ void print_struct_tm(const struct tm *timeinfo) {
     if (!timeinfo)
         return;
 
-    printf("%04d-%02d-%02d %02d:%02d:%02d\n", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1,
+    DEBUG_printf("%04d-%02d-%02d %02d:%02d:%02d\n", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1,
            timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 }
 
